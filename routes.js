f46573d58641a66bfb70c1861b2b98484878fee8
Fernanda Penna <fernandapennaribeiro@gmail.com>
2020-07-01 11:16:49 -0300

    add mobile menu

--- routes.js
+++ routes.js
@@ -10,5 +10,9 @@ routes.get('/', function (req, res) {
     return res.render('index')
 })
 
+routes.get('/test', function (req, res) {
+    return res.render('index-test')
+})
+
 
 module.exports = routes;
